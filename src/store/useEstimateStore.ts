8dd03361d6f1716e82841c26908eaf0855b4a636
techpunk <60296853+schartrand77@users.noreply.github.com>
2025-07-03 18:16:23 -0400

    Add form and result state to estimate store

--- src/store/useEstimateStore.ts
+++ src/store/useEstimateStore.ts
@@ -8,14 +8,39 @@ export interface ModelEstimate {
   [key: string]: unknown
 }
 
+export interface EstimateForm {
+  x_mm: number | string
+  y_mm: number | string
+  z_mm: number | string
+  filament_type: 'pla' | 'petg'
+  print_profile: 'standard' | 'quality' | 'elite'
+}
+
+export interface EstimateResult {
+  estimated_time_minutes: number
+  estimated_cost_usd: number
+}
+
 interface EstimateStoreState {
   selectedModels: ModelEstimate[]
+  form: EstimateForm
+  result: EstimateResult | null
   setSelectedModels: (models: ModelEstimate[]) => void
   clearSelectedModels: () => void
+  setForm: <K extends keyof EstimateForm>(field: K, value: EstimateForm[K]) => void
+  setResult: (result: EstimateResult | null) => void
 }
 
 export const useEstimateStore = create<EstimateStoreState>((set) => ({
   selectedModels: [],
+  form: {
+    x_mm: '',
+    y_mm: '',
+    z_mm: '',
+    filament_type: 'pla',
+    print_profile: 'standard',
+  },
+  result: null,
 
   setSelectedModels: (models: ModelEstimate[]) => {
     console.debug('[useEstimateStore] setSelectedModels:', models)
@@ -25,5 +50,15 @@ export const useEstimateStore = create<EstimateStoreState>((set) => ({
   clearSelectedModels: () => {
     console.debug('[useEstimateStore] clearSelectedModels()')
     set({ selectedModels: [] })
+  },
+
+  setForm: (field, value) => {
+    console.debug('[useEstimateStore] setForm:', field, value)
+    set((state) => ({ form: { ...state.form, [field]: value } }))
+  },
+
+  setResult: (result) => {
+    console.debug('[useEstimateStore] setResult:', result)
+    set({ result })
   },
 }))
