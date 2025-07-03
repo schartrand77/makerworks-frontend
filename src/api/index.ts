b6cfb4b539a3f3b1b77dffd3f6e624574be64ea2
techpunk <60296853+schartrand77@users.noreply.github.com>
2025-07-03 17:31:28 -0400

    Comment out exports for nonexistent modules

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 export * from './auth'
 export * from './users'
-export * from './models'
-export * from './checkout'
-export * from './favorites'
-export * from './filaments'
-export * from './upload'
+// export * from './models' // Module does not exist
+// export * from './checkout' // Module does not exist
+// export * from './favorites' // Module does not exist
+// export * from './filaments' // Module does not exist
+// export * from './upload' // Module does not exist
 export * from './estimate'
