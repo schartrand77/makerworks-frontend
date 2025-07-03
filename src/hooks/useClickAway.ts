be93ae6b943dc6217588e0f23ce9520e13778689
techpunk <60296853+schartrand77@users.noreply.github.com>
2025-07-03 19:17:00 -0400

    refactor: type click away ref

--- src/hooks/useClickAway.ts
+++ src/hooks/useClickAway.ts
@@ -2,11 +2,11 @@
 import { useEffect, useRef } from 'react'
 
 const useClickAway = (onClickAway: () => void) => {
-  const ref = useRef(null)
+  const ref = useRef<HTMLElement | null>(null)
 
   useEffect(() => {
     const handler = (e: MouseEvent) => {
-      if (ref.current && !(ref.current as any).contains(e.target)) {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
         onClickAway()
       }
     }
